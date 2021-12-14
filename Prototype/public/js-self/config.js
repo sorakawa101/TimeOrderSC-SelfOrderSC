268063d0788ccd7aec648852e63d61c783b732d3
s1810289-uec <s1810289@edu.cc.uec.ac.jp>
2021-12-14 19:16:27 +0900

    groupb,c,dのメンバー選択をコメントアウト/Prototypeのファイル名を変更

--- Prototype/public/js-self/config.js
+++ Prototype/public/js-self/config.js
@@ -7,12 +7,12 @@ from "https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js";
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-    apiKey: "AIzaSyBmu_4N4-3bvPwTK6yj6us9c2dP6Gh9rnw",
-    authDomain: "debug-a2265.firebaseapp.com",
-    projectId: "debug-a2265",
-    storageBucket: "debug-a2265.appspot.com",
-    messagingSenderId: "410903637684",
-    appId: "1:410903637684:web:6873e07a235deadd34f1e1"
+    apiKey: "AIzaSyCDNnO-91JzJRKOjXLZGGKOuwip2Op3p70",
+    authDomain: "prototype-96226.firebaseapp.com",
+    projectId: "prototype-96226",
+    storageBucket: "prototype-96226.appspot.com",
+    messagingSenderId: "690970283986",
+    appId: "1:690970283986:web:dadf1cb10b32b56db6420b"
 };
 
 
@@ -20,9 +20,9 @@ export const firebaseConfig = {
 export const app = initializeApp(firebaseConfig); // Keyを使ってFirebaseに接続
 export const db = getDatabase(app); // RealTimeDatabaseに接続
 
-export const dbRefChat = ref(db, "time-order/chat");
-export const dbRefInteract = ref(db, "time-order/interact");
-export const dbRefLog = ref(db, "time-order/log");
-export const dbRefArchive = ref(db, "time-order/archive");
-export const dbRefSetting = ref(db, "time-order/setting");
-export const dbRefUser = ref(db, "time-order/user");
+export const dbRefChat = ref(db, "self-order/chat");
+export const dbRefInteract = ref(db, "self-order/interact");
+export const dbRefLog = ref(db, "self-order/log");
+export const dbRefArchive = ref(db, "self-order/archive");
+export const dbRefSetting = ref(db, "self-order/setting");
+export const dbRefUser = ref(db, "self-order/user");
